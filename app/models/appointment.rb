164d319f1ba7dcde3dfd462317fd48548afc81b5
Jacob <jbmenashi@gmail.com>
2019-04-29 16:37:02 -0400

    complete

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,7 +4,7 @@ class Appointment < ApplicationRecord
    validate :no_overlap
 
    def no_overlap
-      same_schedule = Appointment.select(schedule_id: self.schedule_id)
+      same_schedule = Appointment.select { |app| app.schedule.id == self.schedule.id }
       for appointment in same_schedule do
          if (self.start_time >= appointment.start_time && self.start_time <= appointment.end_time) || (self.end_time >= appointment.start_time && self.end_time <= appointment.end_time)
             errors.add(:start_time, "cannot overlap appointment")
